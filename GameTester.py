7f9cac7f0d67d1a74d2185267839de05604bdff8
JohnVsk <johnvsk@gmail.com>
2018-02-11 20:35:17 +0200

    .

--- GameTester.py
+++ GameTester.py
@@ -7,12 +7,32 @@ import uuid
 
 
 deck1 = Deck(uuid.uuid4(), 'Player1 Deck')
+deck2 = Deck(uuid.uuid4(), 'Player2 Deck')
 
 for card in cardPool:
     deck1.addCardToDeck(card)
+    deck2.addCardToDeck(card)
 
 deck1.shuffleDeck()
-deck1.printDeck()
+deck2.shuffleDeck()
 
-card = deck1.deck.pop()
-print(card.name)
+player1 = Player(
+    name='John',
+    health=30,
+    deck=deck1
+)
+
+player2 = Player(
+    name='Nick',
+    health=30,
+    deck=deck2
+)
+
+print('Player 1 Deck')
+player1.deck.printDeck()
+print('Player 2 Deck')
+player2.deck.printDeck()
+
+# for x in range(0, 2):
+#     card = deck1.drawCardFromTopOfDeck()
+#     print(card.name)
