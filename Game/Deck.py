85b6241a5d62cb50a79d8ba61882354fe03f8814
JohnVsk <johnvsk@gmail.com>
2018-02-11 19:58:08 +0200

    .

--- Game/Deck.py
+++ Game/Deck.py
@@ -1,13 +1,28 @@
 from random import shuffle
+from collections import deque
 
 class Deck:
 
-    def __init__(self, deck_id):
+    def __init__(self, deck_id, deck_name):
         self.deck_id = deck_id
-        self.deck = []
+        self.deck_name = deck_name
+        self.deck = deque([])
 
     def addCardToDeck(self, card):
         self.deck.append(card)
 
     def shuffleDeck(self):
         shuffle(self.deck)
+
+    def drawCardFromTopOfDeck(self):
+        card = self.deck.popleft()
+        return card
+
+    def drawCardFromBottomOfDeck(self):
+        card = self.deck.pop()
+        return card
+
+
+    def printDeck(self):
+        for card in self.deck:
+            print('{0},{1}'.format(card.id, card.name))
--- GameTester.py
+++ GameTester.py
@@ -6,17 +6,13 @@ from Game.Deck import Deck
 import uuid
 
 
-deck1 = Deck(uuid.uuid4())
+deck1 = Deck(uuid.uuid4(), 'Player1 Deck')
 
 for card in cardPool:
     deck1.addCardToDeck(card)
 
-for card in deck1.deck:
-    print('{0},{1}'.format(card.id, card.name))
-
 deck1.shuffleDeck()
+deck1.printDeck()
 
-print('--------------------------------')
-
-for card in deck1.deck:
-    print('{0},{1}'.format(card.id, card.name))
+card = deck1.deck.pop()
+print(card.name)
